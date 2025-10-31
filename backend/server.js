dc552a6d336487825d8842b3ac91c00ccd56b0e3
Aamir377300 <belalaamirkhan@gmail.com>
2025-10-31 22:51:45 +0530

    cors-issue-in-deployment

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,13 @@ const eventRegister = require('./routes/eventRegister');
 
 
 
-
+app.use(
+    cors({
+      origin: process.env.FRONTEND_URL,
+      methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+      credentials: true,
+    })
+  );
 
 
 app.use(cors())
